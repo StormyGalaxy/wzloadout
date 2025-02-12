5cf793f4b491ea76abb3fc294a8a80e8ee799ca4
Andrew Elbaneh <aelbaneh0615@gmail.com>
2025-02-11 21:03:05 -0500

    Created where we droppin section for reload pages venture and oasis

--- components/changelog/Changelog2025.tsx
+++ components/changelog/Changelog2025.tsx
@@ -6,6 +6,18 @@ function Changelog2025() {
       <Row>
         <Col>
           <p>
+            2/11/2025:{" "}
+            <a href="where-we-droppin/reload/venture">
+              Fortnite Reload: Venture:
+            </a>{" "}
+            Where We Droppin is now available!
+          </p>
+          <p>
+            2/11/2025:{" "}
+            <a href="where-we-droppin/reload/oasis">Fortnite Reload: Oasis:</a>{" "}
+            Where We Droppin is now available!
+          </p>
+          <p>
             2/11/2025: <a href="where-we-droppin/og/season-two">OG Season 2:</a>{" "}
             Where We Droppin is now available!
           </p>
@@ -14,7 +26,10 @@ function Changelog2025() {
             Where We Droppin is now available!
           </p>
           <p>
-            2/11/2025: <a href="where-we-droppin/chapter-six/season-one">Chapter 6 Season 1:</a>{" "}
+            2/11/2025:{" "}
+            <a href="where-we-droppin/chapter-six/season-one">
+              Chapter 6 Season 1:
+            </a>{" "}
             Where We Droppin is now available!
           </p>
         </Col>
