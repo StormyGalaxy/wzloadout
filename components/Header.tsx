1b2e91093fbaaa1c22bd5a7c4c1b54ba7b86dd18
aelbaneh0615 <aelbaneh0615@gmail.com>
2025-02-19 20:50:54 +0000

    [ISSUE-5] Implemented header change and added github feeback link generator as a helper function

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { generateGithubLink } from "@/helpers/_silabs/generateGithubLink";
 
 interface HeaderProps {
   className?: string;
   navLinks?: { label: string; href: string; target?: string }[];
+  darkLinks?: boolean;
 }
 
 const defaultNavLinks = [
@@ -16,7 +18,22 @@ const defaultNavLinks = [
 ];
 
 function Header(props: HeaderProps) {
-  const { className, navLinks = defaultNavLinks } = props;
+  const [isLoading, setIsLoading] = useState(true);
+  const { className, navLinks = defaultNavLinks, darkLinks = false } = props;
+  
+  const params = {
+    title: "My New Issue",
+    body: "This is a detailed description.\nWith multiple lines.",
+    labels: ["bug", "help wanted"],
+    assignees: "@Bana0615",
+    milestone: 1,
+    //template: "bug_report.md"
+  };
+  useEffect(() => {
+    const feedbackLink = generateGithubLink("Bana0615", "bootstrap-nextjs-github-pages", params);
+    defaultNavLinks.push({ label: "Feedback", href: feedbackLink, target: "_blank" });
+    setIsLoading(false);
+  }, []);
 
   return (
     <Navbar
@@ -28,7 +45,7 @@ function Header(props: HeaderProps) {
     >
       <Container>
         <Navbar.Brand href="/">bootstrap-nextjs-github-pages</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className={darkLinks ? 'black-toggler' : ""} />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             {navLinks.map((link, index) => (
