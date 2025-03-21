9511c94fbd2a8df0a10f5e6a2bc265f04c9b5f3e
Bana0615 <aelbaneh0615@gmail.com>
2025-03-21 19:43:21 +0000

    Adding save and delete helper functions to db

---
+++ helpers/_silabs/pouchDb/save.ts
@@ -0,0 +1,60 @@
+import { v4 as uuidv4 } from "uuid";
+import Joi from "joi"; // Ensure you have Joi installed
+
+export const save = async (
+  db: PouchDB.Database,
+  data: any,
+  dbSchema: Joi.ObjectSchema,
+  type: string
+) => {
+  if (db) {
+    try {
+      const { error, value: validatedData } = dbSchema.validate(data);
+
+      if (error) {
+        console.error(`Validation error for ${type}:`, error.details);
+        return { success: false, error: error.details };
+      }
+
+      let doc = { ...validatedData };
+
+      if (data._id && data._rev) {
+        try {
+          const existingDoc = await db.get(data._id);
+          doc._id = data._id;
+          doc._rev = existingDoc._rev;
+        } catch (getErr: unknown) {
+          if (getErr instanceof Error && getErr.name === "NotFoundError") {
+            doc._id = uuidv4();
+          } else {
+            console.error(
+              `Error getting existing ${type} document for update:`,
+              getErr
+            );
+            return {
+              success: false,
+              error: `Error getting ${type} document for update.`,
+            };
+          }
+        }
+      } else {
+        doc._id = data._id ? data._id : uuidv4();
+      }
+
+      const response = await db.put(doc);
+      return { success: true, data: response };
+    } catch (error: unknown) {
+      console.error(`Error adding ${type}:`, error);
+
+      if (error instanceof Error) {
+        return { success: false, error: error.message };
+      } else if (typeof error === "string") {
+        return { success: false, error: error };
+      } else {
+        return { success: false, error: "An unknown error occurred." };
+      }
+    }
+  } else {
+    return { success: false, error: "Database not initialized" };
+  }
+};
