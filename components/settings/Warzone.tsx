baa3694dfc5c933f7b08768ce1702cae3738a044
aelbaneh0615 <aelbaneh0615@gmail.com>
2025-04-01 15:30:17 +0000

    [ISSUE-11] cleaned up warzone settings page

--- components/settings/Warzone.tsx
+++ components/settings/Warzone.tsx
@@ -1,108 +1,73 @@
 import React, { useState, useEffect } from "react";
-import { Row, Col, Form, Button } from "react-bootstrap";
+import { Row, Col, Form } from "react-bootstrap";
 //Components
 import CustomAlert from "@/components/_silabs/bootstrap/CustomAlert";
 //styles
 import styles from "@/public/styles/components/Settings.module.css";
-//DB
-import getAllSettings from "@/helpers/database/settings/getAllSettings";
-import saveSettings from "@/helpers/database/settings/saveSettings";
-import { useDatabase } from "@/contexts/DatabaseContext";
 //Types
 import { sclSettings } from "@/types/_fw";
 
 const warzoneGames = ["Black Ops 6", "Modern Warfare 3", "Modern Warfare 2"];
 const warzoneTypes = ["Primary", "Secondary", "Melee"];
 
-export default function Warzone() {
-  const { dbs, isReady } = useDatabase();
-  const [isSpinning, setIsSpinning] = useState(false);
+interface WarzoneProps {
+  db: PouchDB.Database;
+  settingsData: sclSettings;
+  onDataChange: (updatedData: sclSettings) => void;
+}
+
+export default function Warzone({ db, settingsData, onDataChange }: WarzoneProps) {
   const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState<sclSettings>({});
+  const [wzData, setWzData] = useState<sclSettings>(settingsData || {});
   const [showAlert, setShowAlert] = useState(false);
   const [alertVariant, setAlertVariant] = useState("success");
   const [alertMessage, setAlertMessage] = useState("");
 
   useEffect(() => {
-    async function fetchData() {
-      if (dbs.settings) {
-        try {
-          const allData = await getAllSettings(dbs.settings);
-          // Initialize if not present in the database
-          if (!allData.wzPrimaryWeapons) {
-            allData.wzPrimaryWeapons = {};
-            allData.wzSecondaryWeapons = {};
-            allData.wzMeleeWeapons = {};
-            warzoneGames.forEach((header) => {
-              allData.wzPrimaryWeapons[header] = true;
-              allData.wzSecondaryWeapons[header] = true;
-              allData.wzMeleeWeapons[header] = true;
-            });
-          }
-          setData(allData);
-        } catch (err: unknown) {
-          const errorMessage =
-            err instanceof Error ? err.message : "Failed to fetch settings.";
-          setAlertVariant("danger");
-          setAlertMessage(errorMessage);
-          setShowAlert(true);
-        } finally {
-          setIsLoading(false);
+    if (db) {
+      try {
+        const allData = structuredClone(wzData);
+        if (!allData.wzPrimaryWeapons) {
+          allData.wzPrimaryWeapons = {};
+          allData.wzSecondaryWeapons = {};
+          allData.wzMeleeWeapons = {};
+          warzoneGames.forEach((header) => {
+            allData.wzPrimaryWeapons[header] = true;
+            allData.wzSecondaryWeapons[header] = true;
+            allData.wzMeleeWeapons[header] = true;
+          });
         }
+        onDataChange(allData);
+        setWzData(allData);
+      } catch (err: unknown) {
+        const errorMessage =
+          err instanceof Error ? err.message : "Failed to fetch settings.";
+        setAlertVariant("danger");
+        setAlertMessage(errorMessage);
+        setShowAlert(true);
+      } finally {
+        setIsLoading(false);
       }
     }
-    if (isReady) {
-      fetchData();
-    }
-  }, [dbs, isReady]);
+  }, [db]);
 
   const handleCheckboxChange = (
     type: string,
     header: string,
     isChecked: boolean
   ) => {
-    setData((prevData) => {
+    setWzData((prevData) => {
       const newData = { ...prevData };
       if (!newData[`wz${type}Weapons`]) {
         newData[`wz${type}Weapons`] = {};
       }
       newData[`wz${type}Weapons`][header] = isChecked;
+      onDataChange(newData);
       return newData;
     });
   };
 
-  const save = async () => {
-    if (!dbs.settings) {
-      console.error("Database is not initialized.");
-      setAlertVariant("danger");
-      setAlertMessage("Database is not initialized.");
-      setShowAlert(true);
-
-      return;
-    }
-
-    setIsSpinning(true);
-
-    try {
-      await saveSettings(dbs.settings, data);
-      setAlertVariant("success");
-      setAlertMessage("Settings Saved!");
-      setShowAlert(true);
-    } catch (error: unknown) {
-      console.error("Error saving settings:", error);
-      const errorMessage =
-        error instanceof Error
-          ? error.message
-          : "An unknown error occurred while saving settings.";
-      setAlertVariant("danger");
-      setAlertMessage(errorMessage);
-      setShowAlert(true);
-    } finally {
-      setIsSpinning(false);
-    }
-  };
-
-  if (!isReady || isLoading) {
+  if (isLoading) {
     return <div className="text-center">Loading database...</div>;
   }
 
@@ -132,7 +97,7 @@ export default function Warzone() {
                       id={`checkbox-${header.replace(/\s/g, "")}-${type}`} // Added type to the ID
                       label={header}
                       className={`me-2 custom-checkbox ${styles.wzCheckbox}`}
-                      checked={data[`wz${type}Weapons`]?.[header] || false} // Use optional chaining
+                      checked={wzData[`wz${type}Weapons`]?.[header] || false} // Use optional chaining
                       onChange={(e) =>
                         handleCheckboxChange(type, header, e.target.checked)
                       }
@@ -143,20 +108,6 @@ export default function Warzone() {
             </Row>
           </React.Fragment>
         ))}
-        <Row className="mt-5 justify-content-center">
-          <Col xs={12} sm={6} md={3}>
-            <div className="d-flex justify-content-center">
-              <Button
-                variant="primary"
-                className="w-100"
-                disabled={isSpinning}
-                onClick={save}
-              >
-                Save Settings
-              </Button>
-            </div>
-          </Col>
-        </Row>
       </Col>
     </Row>
   );
--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,12 +1,89 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Head from "next/head";
-import { Container, Row, Col, Tabs, Tab } from "react-bootstrap";
+import { Container, Row, Col, Tabs, Tab, Button } from "react-bootstrap";
 import Header from "@/components/Header";
 //Components
+import CustomAlert from "@/components/_silabs/bootstrap/CustomAlert";
 import Warzone from "@/components/settings/Warzone";
+//DB
+import getAllSettings from "@/helpers/database/settings/getAllSettings";
+import saveSettings from "@/helpers/database/settings/saveSettings";
+import { useDatabase } from "@/contexts/DatabaseContext";
+//Types
+import { sclSettings } from "@/types/_fw";
 
 export default function Settings() {
+  const { dbs, isReady } = useDatabase();
   const [key, setKey] = useState<string>("warzone");
+  const [data, setData] = useState<sclSettings>({});
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertVariant, setAlertVariant] = useState("success");
+  const [alertMessage, setAlertMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [isSpinning, setIsSpinning] = useState(false);
+
+
+  useEffect(() => {
+    async function fetchData() {
+      if (dbs.settings) {
+        try {
+          const allData = await getAllSettings(dbs.settings);
+          setData(allData);
+        } catch (err: unknown) {
+          const errorMessage =
+            err instanceof Error ? err.message : "Failed to fetch settings.";
+          setAlertVariant("danger");
+          setAlertMessage(errorMessage);
+          setShowAlert(true);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+    }
+    if (isReady) {
+      fetchData();
+    }
+  }, [dbs, isReady]);
+
+
+  const save = async () => {
+    if (!dbs.settings) {
+      console.error("Database is not initialized.");
+      setAlertVariant("danger");
+      setAlertMessage("Database is not initialized.");
+      setShowAlert(true);
+
+      return;
+    }
+
+    setIsSpinning(true);
+
+    try {
+      await saveSettings(dbs.settings, data);
+      setAlertVariant("success");
+      setAlertMessage("Settings Saved!");
+      setShowAlert(true);
+    } catch (error: unknown) {
+      console.error("Error saving settings:", error);
+      const errorMessage =
+        error instanceof Error
+          ? error.message
+          : "An unknown error occurred while saving settings.";
+      setAlertVariant("danger");
+      setAlertMessage(errorMessage);
+      setShowAlert(true);
+    } finally {
+      setIsSpinning(false);
+    }
+  };
+
+  const handleWarzoneDataChange = (updatedData: sclSettings) => {
+    setData(updatedData);
+  };
+
+  if (!isReady || isLoading) {
+    return <div className="text-center">Loading database...</div>;
+  }
 
   return (
     <>
@@ -14,10 +91,16 @@ export default function Settings() {
         <title>Settings - Call Of Duty Random Class Generator</title>
       </Head>
       <Header />
-      <Container fluid>
+      <Container className="shadow-lg p-3 mt-4 bg-body rounded">
         <Row>
           <Col>
-            <Container className="shadow-lg p-3  mt-4 bg-body rounded">
+            <CustomAlert
+              variant={alertVariant}
+              message={alertMessage}
+              show={showAlert}
+              onClose={() => setShowAlert(false)}
+            />
+            <Container>
               <Tabs
                 id="controlled-tab-example"
                 activeKey={key}
@@ -25,10 +108,24 @@ export default function Settings() {
                 className="mb-3"
               >
                 <Tab eventKey="warzone" title="Warzone">
-                  <Warzone />
+                  <Warzone db={dbs.settings} settingsData={data} onDataChange={handleWarzoneDataChange} />
                 </Tab>
               </Tabs>
             </Container>
+          </Col>
+        </Row>
+        <Row className="mt-5 justify-content-center">
+          <Col xs={12} sm={6} md={3}>
+            <div className="d-flex justify-content-center">
+              <Button
+                variant="success"
+                className="w-100"
+                disabled={isSpinning}
+                onClick={save}
+              >
+                Save Settings
+              </Button>
+            </div>
           </Col>
         </Row>
       </Container>
