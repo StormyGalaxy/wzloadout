18c35c04d6a5c07a8eb89751a52756253306f2e4
Andrew Elbaneh <aelbaneh0615@gmail.com>
2025-03-31 23:09:45 -0400

    SclPlaceholder can now handle html being sent in

--- components/_silabs/SclPlaceholder.tsx
+++ components/_silabs/SclPlaceholder.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ContentLoader from "react-content-loader";
 
-const SclPlaceholder = ({ isLoading, value }) =>
-  isLoading ? (
+const SclPlaceholder = ({ isLoading, value }) => {
+  const containsHTML =
+    /<(?!(?:area|br|col|embed|hr|img|input|link|meta|param|source|track|wbr)\b)[a-z][^>]*>/i.test(
+      value
+    );
+
+  return isLoading ? (
     <ContentLoader
       height={30}
       width={65}
@@ -13,8 +18,11 @@ const SclPlaceholder = ({ isLoading, value }) =>
     >
       <rect x="0" y="5.5" rx="4" ry="4" width="65" height="15" />
     </ContentLoader>
+  ) : containsHTML ? (
+    <div dangerouslySetInnerHTML={{ __html: value }} />
   ) : (
     <>{value}</>
   );
+};
 
 export default SclPlaceholder;
