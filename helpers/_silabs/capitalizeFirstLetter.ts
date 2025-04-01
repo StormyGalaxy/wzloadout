cf2589f6e6dc17ad1af513ba4103330f169e3285
aelbaneh0615 <aelbaneh0615@gmail.com>
2025-04-01 13:53:40 +0000

    created capitalizeFirstLetter function

---
+++ helpers/_silabs/capitalizeFirstLetter.ts
@@ -0,0 +1,6 @@
+export function capitalizeFirstLetter(value: string, delimiter: string) {
+   return value
+      .split(delimiter)
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ")
+}
