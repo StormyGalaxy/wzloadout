80800c0b190356d69fbf35a98322c7e1b98a4404
Andrew Elbaneh <aelbaneh0615@gmail.com>
2025-04-14 13:51:48 -0400

    [NextJS Conversion] Converted the vanguard generator page

---
+++ app/vanguard/generator/page.tsx
@@ -0,0 +1,68 @@
+import { Container, Row, Col } from "react-bootstrap";
+import type { Metadata } from "next";
+// --- Layout ---
+import PageLayout from "@/components/PageLayout";
+// --- Loadout ---
+import VanguardLoadout from "@/components/generators/vanguard/VanguardLoadout";
+
+// --- Metadata ---
+export const metadata: Metadata = {
+  title: "Vanguard",
+  description:
+    "Spice up your COD gameplay! Generate unique random loadouts for Call of Duty Vanguard. Discover new weapons, perks, and gear combinations.",
+  keywords: [
+    "Call of duty",
+    "call",
+    "of",
+    "duty",
+    "cod",
+    "call of duty",
+    "random",
+    "class",
+    "generator",
+    "random class generator",
+    "rcg",
+    "free",
+    "mp",
+    "multiplayer",
+    "call of duty random class generator",
+    "COD Vanguard RCG",
+    "vanguard random class generator",
+    "vanguard",
+    "vanguard rcg",
+    "vanguard random class generator",
+    "class generator",
+    "zombies",
+    "treyarch zombies",
+    "vanguard zombies",
+    "vanguard rcg",
+    "vanguard random class generator",
+  ],
+};
+
+const navLinks = [
+  { label: "Home", href: "/" },
+  { label: "Zombies Generator", href: "/vanguard/zombies-generator" },
+  { label: "Loadout Info", href: "/vanguard/info" },
+  { label: "Changelog", href: "/changelog" },
+];
+
+export default function VanguardGeneratorPage() {
+  return (
+    <PageLayout navLinks={navLinks} headerClassName="vanguard">
+      <Container>
+        <h2 className="text-center mb-4">
+          Vanguard
+          <span className="d-none d-sm-inline-block">&nbsp;-&nbsp;</span>
+          <br className="d-block d-sm-none" />
+          Random Class Generator
+        </h2>
+        <Row className="shadow-lg p-3 bg-body rounded mb-4">
+          <Col className="mx-auto">
+            <VanguardLoadout />
+          </Col>
+        </Row>
+      </Container>
+    </PageLayout>
+  );
+}
