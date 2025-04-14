c02fe17ebaa2f5b3dc2e2eececa3f9792a94f9e5
Andrew Elbaneh <aelbaneh0615@gmail.com>
2025-04-14 15:01:45 -0400

    [NextJS Conversion] Vanguard: Converted the weapons info section page

---
+++ components/info/WeaponList.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+// --- Components ---
+import InfoList from "@/components/info/InfoList";
+// --- Helpers ---
+import { getWeapon } from "@/helpers/info/getWeapon";
+
+interface ListProps {
+  game: string;
+  dataKeys?: Array<string>;
+}
+
+const defaultDataKeys = ["name", "type", "game", "no_attach"];
+
+export default function WeaponList({
+  game,
+  dataKeys = defaultDataKeys,
+}: ListProps) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState({});
+  const [types, setTypes] = useState<string[]>([]);
+
+  useEffect(() => {
+    const tmp_types: string[] = [];
+    const dataList = getWeapon(game);
+    setData(dataList);
+
+    //Format data
+    for (const key in dataList) {
+      const type = dataList[key].type;
+
+      if (!tmp_types.includes(type)) {
+        tmp_types.push(type);
+      }
+    }
+    setTypes(tmp_types);
+
+    setIsLoading(false);
+  }, []);
+  return (
+    <>
+      {!isLoading && (
+        <InfoList
+          data={data}
+          dataKeys={dataKeys}
+          types={types}
+          url="/vanguard/info/weapon"
+        />
+      )}
+    </>
+  );
+}
