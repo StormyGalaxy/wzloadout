1b87b1c229689a276a64ca24702b79e385aaab47
aelbaneh0615 <aelbaneh0615@gmail.com>
2025-04-18 17:22:05 +0000

    [NextJS Conversion] Modern Warfare Three: finished conversion

---
+++ app/modern-warfare/three/info/weapon/page.tsx
@@ -0,0 +1,79 @@
+import { Suspense } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+// --- Next ---
+import type { Metadata } from "next";
+// --- Layout ---
+import PageLayout from "@/components/PageLayout";
+// --- Components ---
+import WeaponDisplayClient from "@/components/info/WeaponDisplayClient";
+
+type Props = {
+  params: {};
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+// --- Dynamic Metadata Generation ---
+export async function generateMetadata({
+  searchParams,
+}: Props): Promise<Metadata> {
+  const weaponName = searchParams?.value || "Unknown Weapon";
+  const displayWeaponName = Array.isArray(weaponName)
+    ? weaponName[0]
+    : weaponName;
+  const gameName = "Modern Warfare Three";
+
+  return {
+    title: `${gameName} Weapon - ${displayWeaponName}`,
+    description: `View information and all available attachments for the ${displayWeaponName} weapon in Call of Duty: ${gameName}.`,
+    keywords: [
+      "Call of duty",
+      gameName.toLowerCase(),
+      "weapon",
+      "info",
+      "attachments",
+      displayWeaponName,
+      "random class generator",
+      "rcg",
+      "cod",
+      "loadout",
+      "multiplayer",
+      "zombies",
+    ],
+  };
+}
+
+const navLinks = [
+  { label: "Home", href: "/" },
+  {
+    label: "Multiplayer Generator",
+    href: "/modern-warfare/three/generator",
+  },
+  {
+    label: "Zombies Generator",
+    href: "/modern-warfare/three/zombies-generator",
+  },
+  { label: "Loadout Info", href: "/modern-warfare/three/info" },
+  { label: "Changelog", href: "/changelog" },
+];
+
+export default function ModernWarfareThreeWeaponPage() {
+  const game = "modern-warfare-three";
+
+  return (
+    <PageLayout navLinks={navLinks} headerClassName="modern-warfare">
+      <Container fluid>
+        <Row>
+          <Suspense
+            fallback={
+              <Col>
+                <p className="text-center">Loading page...</p>
+              </Col>
+            }
+          >
+            <WeaponDisplayClient game={game} />
+          </Suspense>
+        </Row>
+      </Container>
+    </PageLayout>
+  );
+}
