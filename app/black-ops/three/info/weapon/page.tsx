e2ca1fcaf706ff20539ff99883e135e1cff80b5a
Andrew Elbaneh <aelbaneh0615@gmail.com>
2025-04-19 16:39:28 -0400

    [NextJS Conversion] Black Ops Three: finished conversion

---
+++ app/black-ops/three/info/weapon/page.tsx
@@ -0,0 +1,72 @@
+import { Suspense } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+// --- Next ---
+import type { Metadata } from "next";
+// --- Layout ---
+import PageLayout from "@/components/PageLayout";
+// --- Components ---
+import WeaponDisplayClient from "@/components/info/WeaponDisplayClient";
+
+type Props = {
+  params: object;
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+// --- Dynamic Metadata Generation ---
+export async function generateMetadata({
+  searchParams,
+}: Props): Promise<Metadata> {
+  const weaponName = searchParams?.value || "Unknown Weapon";
+  const displayWeaponName = Array.isArray(weaponName)
+    ? weaponName[0]
+    : weaponName;
+  const gameName = "Black Ops Three";
+
+  return {
+    title: `${gameName} Weapon - ${displayWeaponName}`,
+    description: `View information and all available attachments for the ${displayWeaponName} weapon in Call of Duty: ${gameName}.`,
+    keywords: [
+      "Call of duty",
+      gameName.toLowerCase(),
+      "weapon",
+      "info",
+      "attachments",
+      displayWeaponName,
+      "random class generator",
+      "rcg",
+      "cod",
+      "loadout",
+      "multiplayer",
+      "zombies",
+    ],
+  };
+}
+
+const navLinks = [
+  { label: "Home", href: "/" },
+  { label: "Multiplayer Generator", href: "/black-ops/three/generator" },
+  { label: "Loadout Info", href: "/black-ops/three/info" },
+  { label: "Changelog", href: "/changelog" },
+];
+
+export default function BlackOpsThreeWeaponPage() {
+  const game = "black-ops-three";
+
+  return (
+    <PageLayout navLinks={navLinks} headerClassName="black-ops">
+      <Container fluid>
+        <Row>
+          <Suspense
+            fallback={
+              <Col>
+                <p className="text-center">Loading page...</p>
+              </Col>
+            }
+          >
+            <WeaponDisplayClient game={game} />
+          </Suspense>
+        </Row>
+      </Container>
+    </PageLayout>
+  );
+}
