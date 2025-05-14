2e40aaacdcd500d62547af5c9939bafc53a2eef8
Bana0615 <aelbaneh0615@gmail.com>
2025-05-14 12:45:12 -0400

    sitemap and robots.txt are generated directly to the out directly

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,5 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://pages.silocitylabs.com',
   generateRobotsTxt: true,
+  outDir: './out', // Output directory for the generated sitemap
 };
