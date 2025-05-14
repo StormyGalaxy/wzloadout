985494d8483b6ca75d889c0ad375340b3d1d2398
Bana0615 <aelbaneh0615@gmail.com>
2025-05-14 13:27:06 -0400

    [structured-data] Created helpers and a component to create/define structured data using @silocitypages/utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,4 @@
+// --- General Helpers ---
 export { capitalizeFirstLetter } from './helpers/capitalizeFirstLetter';
 export { generateGithubLink } from './helpers/generateGithubLink';
 export { getLocalStorage, setLocalStorage } from './helpers/localStorage';
@@ -6,3 +7,18 @@ export { randomListItem } from './helpers/randomListItem';
 
 // --- Analytics Utils ---
 export { sendEvent } from './analytics/gtag';
+
+// --- SEO Components ---
+export { default as StructuredData } from './components/StructuredData';
+
+// --- SEO Helpers ---
+export {
+  generateOrganizationSchema,
+  generateWebSiteSchema,
+  generateWebPageSchema,
+  generateArticleSchema,
+  generateBreadcrumbListSchema,
+  generatePersonSchema,
+  generateVideoObjectSchema,
+  generateFAQPageSchema,
+} from './helpers/schemaGenerators';
