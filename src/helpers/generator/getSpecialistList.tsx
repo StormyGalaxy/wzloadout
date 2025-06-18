05398ec8d512f7aad53ad06f55b9ce54739a2b57
Bana0516 <aelbaneh0615@gmail.com>
2025-06-18 13:52:21 -0400

    Fixed linting errors on helpers for getSpecialistList

--- src/helpers/generator/getSpecialistList.tsx
+++ src/helpers/generator/getSpecialistList.tsx
@@ -1,13 +1,18 @@
+// --- Data ---
 import bo3List from '@/json/black-ops/three/specialists.json';
 import bo4List from '@/json/black-ops/four/specialists.json';
 import iwList from '@/json/infinite-warfare/combat-rig.json';
+// --- Types ---
+import { Specialist } from '@/types/Generator';
 
-const data: Record<string, any> = {
+type ItemList = Specialist[] | Record<string, Specialist>;
+
+const data: Record<string, ItemList> = {
   'black-ops-three': bo3List,
   'black-ops-four': bo4List,
   'infinite-warfare': iwList,
 };
 
-export function getSpecialistList(game: string): any {
+export function getSpecialistList(game: string): ItemList {
   return data[game] || {};
 }
