f9e360fe5dd3abda782df53451906ac10f9461fe
Andrew Elbaneh <aelbaneh0615@gmail.com>
2025-07-10 12:48:30 -0400

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://pages.silocitylabs.com',
+  siteUrl: process.env.NEXT_PUBLIC_APP_URL || 'https://pages.silocitylabs.com',
   generateRobotsTxt: true,
   outDir: './out', // Output directory for the generated sitemap
 };
